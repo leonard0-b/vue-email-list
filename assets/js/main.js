16a9bc8563ba42a70bddb26e43801e73c0a2e7e1
Leonardo <leonardo.bongianni@gmail.com>
2021-04-08 17:01:40 +0200

    Esercizio Completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,15 @@ var app = new Vue ({
     data: {
       email: []
     },
-    methods: {
-      getMail: function () {
-        for (var i = 0; i < 10; i++) {
-          axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-          .then((response) => {
-            this.email.push(response.data.response)
-            console.log(this.email);
-          })
-        }
+    mounted: function () {
+      for (var i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.email.push(response.data.response)
+        })
       }
-    }
+      this.email = [];
+    },
+    methods: {
+    },
 });
